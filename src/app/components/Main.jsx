c4ab3f6d6fbf3112c2ef7ed2c969331e5cb3c494
Milena Kari <milenakari@yahoo.com>
2020-08-16 09:47:26 -1000

    added route guards

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -8,16 +8,22 @@ import {history} from '../store/history';
 import { ConnectedNavigation } from './Navigation'
 import { ConnectedTaskDetail } from './TaskDetail'
 
+const RouteGuard = Component => ({match}) => {
+    console.info("route guard ", match);
+    return <Component match={match}/>
+}
+    
+    
 export const Main = () => (
     <Router history={history}>
         <Provider store={store}>
             <div>
                 <ConnectedNavigation />
                 {/*Make dashbboard only appear if the route has /dashboard */}
-                <Route exact path="/dashboard" render={() => (<ConnectedDashboard/>)}/>
+                <Route exact path="/dashboard" render={RouteGuard(ConnectedDashboard)}/>
                 <Route exact
                        path="/tasks/:id"
-                       render = {({match}) => (<ConnectedTaskDetail match={match} />)}/>
+                       render = {RouteGuard(ConnectedTaskDetail)}/>
             </div>
 
         </Provider>
